5e393c9ca77b0dea84767cc3731500bb2dc9517d
rochafialvin <rochafi.dev@gmail.com>
2021-11-24 15:37:01 +0700

    filter product by name

--- src/pages/Home/components/ProductManager.jsx
+++ src/pages/Home/components/ProductManager.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ProductManager() {
+// props : onSearchProducts
+function ProductManager(props) {
+  const [keyword, setKeyword] = useState("");
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const btnSearchHandler = () => {
+    props.onSearchProducts(keyword);
+  };
+
   return (
     <div className="col-3">
-      <h1>Product Manager</h1>
+      <div className="card">
+        <div className="card-header">
+          <strong>Filter products</strong>
+        </div>
+        <div className="card-body">
+          <label>Product Name</label>
+          <input
+            type="text"
+            className="form-control mb-3"
+            onChange={handleChange}
+            value={keyword}
+          />
+          <label>Product Category</label>
+          <select className="form-control">
+            <option value="">All Items</option>
+            <option value="kaos">Kaos</option>
+            <option value="celana">Celana</option>
+            <option value="aksesoris">Aksesoris</option>
+          </select>
+          <button
+            onClick={btnSearchHandler}
+            className="btn btn-outline-primary mt-3 d-block w-100"
+          >
+            Search
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
