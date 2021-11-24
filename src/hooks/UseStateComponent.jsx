640cbae510a63b237ddbb8baefe8e7adae614cc7
rochafialvin <rochafi.dev@gmail.com>
2021-11-24 10:55:52 +0700

    useState object

--- src/hooks/UseStateComponent.jsx
+++ src/hooks/UseStateComponent.jsx
@@ -1,21 +1,62 @@
 import React, { useState } from "react";
 
 function UseStateComponent() {
-  const [count, setCount] = useState(25);
+  // [nilai, function]
+  // count : menyimpan data
+  // setCount : mengubah data
+  const [count, setCount] = useState(0); // count : 2
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+    setCount((prevCount) => {
+      // 1 + 1 = 2
+      return prevCount + 1;
+    });
+    setCount((prevCount) => {
+      // 1 + 1 = 2
+      return prevCount + 1;
+    });
+    setCount((prevCount) => {
+      // 1 + 1 = 2
+      return prevCount + 1;
+    });
   };
 
   const decrement = () => {
     setCount(count - 1);
   };
 
+  // Object
+  // person : {lastName : "Doe"}
+  const [person, setPerson] = useState({ firstName: "", lastName: "" });
+
+  // "John"
+  const firstNameChange = (e) => {
+    setPerson({ ...person, firstName: e.target.value });
+  };
+  // "Doe"
+  const lastNameChange = (e) => {
+    setPerson({ ...person, lastName: e.target.value });
+  };
+
   return (
-    <div className="container">
+    <div className="container ">
       <h1 className="text-center">useState</h1>
 
-      <div className="d-flex justify-content-around" style={{ width: "200px" }}>
+      <div
+        className="d-flex justify-content-around mt-5 mx-auto"
+        style={{ width: "200px" }}
+      >
+        <input type="text" placeholder="firstName" onChange={firstNameChange} />
+        <input type="text" placeholder="lastName" onChange={lastNameChange} />
+
+        <h5 className="ms-5">{JSON.stringify(person)}</h5>
+      </div>
+
+      {/* <div className="d-flex justify-content-around" style={{ width: "200px" }}>
         <button className="btn btn-outline-primary" onClick={decrement}>
           -
         </button>
@@ -23,7 +64,7 @@ function UseStateComponent() {
         <button className="btn btn-outline-primary" onClick={increment}>
           +
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
