ab888268a96759f00fc34e7d71599d02ea615dfb
rochafialvin <rochafi.dev@gmail.com>
2021-11-26 14:13:28 +0700

    Create product detail page

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -6,7 +6,7 @@ import "./style.css";
 
 // props : product {}
 function ProductCard(props) {
-  const { productName, productImage, price } = props.product;
+  const { id, productName, productImage, price } = props.product;
 
   return (
     <div className="card product-card">
@@ -17,7 +17,13 @@ function ProductCard(props) {
           <span className="text-muted">Rp. {price}</span>
         </div>
         <div className="d-flex flex-row justify-content-end">
-          <Button tag={Link} to="/" outline color="primary">
+          <Button
+            className="mt-2 w-100"
+            tag={Link}
+            to={`/product/${id}`}
+            outline
+            color="primary"
+          >
             Detail
           </Button>
           {/* <button className="btn btn-primary mt-2 w-100">Detail</button> */}
--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "../../utils/axios";
 
 function ProductDetail() {
-  // params : { id : 99 }
   const params = useParams();
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get("/products", { params: { id: params.id } })
+      .then((res) => {
+        setProduct(res.data[0]);
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, []);
+
+  const { productImage, productName, price, description } = product;
   return (
-    <div>
-      <h1>Product Detail : {params.id}</h1>
+    <div className="container">
+      <div className="row mt-3">
+        <div className="col-6">
+          <img style={{ width: "100%" }} src={productImage} alt="" />
+        </div>
+        <div className="col-6 d-flex flex-column justify-content-center">
+          <h4>{productName}</h4>
+          <h5>Rp {price}</h5>
+          <p>{description}</p>
+          <div className="d-flex flex-row align-items-center">
+            <button className="btn btn-primary ">-</button>
+            <strong className="text-center mx-4">{quantity}</strong>
+            <button className="btn btn-primary ">+</button>
+          </div>
+          <button className="btn btn-success mt-3">Add to cart</button>
+        </div>
+      </div>
     </div>
   );
 }
