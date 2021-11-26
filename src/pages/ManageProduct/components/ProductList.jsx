e68440c5be87c0c18c825a171f9d26828d8257e7
rochafialvin <rochafi.dev@gmail.com>
2021-11-26 16:25:32 +0700

    End of day-10

--- src/pages/ManageProduct/components/ProductList.jsx
+++ src/pages/ManageProduct/components/ProductList.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "../../../utils/axios";
 
 function ProductList(props) {
   const { id, productName, productImage, price, description, category } =
     props.product;
+
+  const [isEdit, setIsEdit] = useState(false);
 
   const onBtnDeleteClick = () => {
     axios
@@ -14,6 +16,54 @@ function ProductList(props) {
       })
       .catch((err) => alert("Gagal delete product"));
   };
+
+  const onBtnSaveClick = () => {};
+
+  const onBtnEditClick = () => {
+    setIsEdit(true);
+  };
+  const onBtnCancelClick = () => {
+    setIsEdit(false);
+  };
+
+  if (isEdit) {
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>
+          <input type="text" name="productName" />
+        </td>
+        <td>
+          <input type="text" name="price" />
+        </td>
+        <td>
+          <input type="text" name="productImage" />
+        </td>
+        <td>
+          <input type="text" name="description" />
+        </td>
+        <td>
+          <select name="category" className="form-control">
+            <option value="">All Items</option>
+            <option value="kaos">Kaos</option>
+            <option value="celana">Celana</option>
+            <option value="aksesoris">Aksesoris</option>
+          </select>
+        </td>
+        <td>
+          <button onClick={onBtnCancelClick} className="btn btn-outline-danger">
+            Cancel
+          </button>
+        </td>
+        <td>
+          <button onClick={onBtnSaveClick} className="btn btn-outline-success">
+            Save
+          </button>
+        </td>
+      </tr>
+    );
+  }
+
   return (
     <tr>
       <td>{id}</td>
@@ -25,7 +75,9 @@ function ProductList(props) {
       <td>{description}</td>
       <td>{category}</td>
       <td>
-        <button className="btn btn-outline-secondary">Edit</button>
+        <button onClick={onBtnEditClick} className="btn btn-outline-secondary">
+          Edit
+        </button>
       </td>
       <td>
         <button onClick={onBtnDeleteClick} className="btn btn-outline-danger">
