166e89c6c4c0d777780f247a997b6bd4214e3964
rochafialvin <rochafi.dev@gmail.com>
2021-12-01 16:16:49 +0700

    Add modal transaction

--- src/pages/HistoryTransaction/index.js
+++ src/pages/HistoryTransaction/index.js
@@ -1,10 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../utils/axios";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { useSelector } from "react-redux";
 import "./style.css";
 
+function TransactionModal(props) {
+  return (
+    <Modal isOpen={props.isModalShow}>
+      <ModalHeader>Modal title</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </ModalBody>
+      <ModalFooter>
+        <Button outline color="primary">
+          Do Something
+        </Button>
+        <Button
+          onClick={() => {
+            props.setIsModalShow(false);
+          }}
+          outline
+          color="danger"
+        >
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+}
+
 function HistoryTransaction() {
   const [transactions, setTransactions] = useState([]);
+  const [isModalShow, setIsModalShow] = useState(false);
   const userId = useSelector((state) => state.auth.id);
 
   useEffect(async () => {
@@ -67,7 +100,13 @@ function HistoryTransaction() {
 
             {/* hitam - start */}
             <div className="transaction-detail-link">
-              <span>Lihat detail transaksi</span>
+              <span
+                onClick={() => {
+                  setIsModalShow(!isModalShow);
+                }}
+              >
+                Lihat detail transaksi
+              </span>
             </div>
             {/* hitam - end */}
           </div>
@@ -75,7 +114,15 @@ function HistoryTransaction() {
       );
     });
   };
-  return <div className="transaction-container">{renderTransaction()}</div>;
+  return (
+    <>
+      <div className="transaction-container">{renderTransaction()}</div>
+      <TransactionModal
+        isModalShow={isModalShow}
+        setIsModalShow={setIsModalShow}
+      />
+    </>
+  );
 }
 
 export default HistoryTransaction;
