a7b42e9c354f413a63dd6dd3ac52bb0ec8f5075e
rochafialvin <rochafi.dev@gmail.com>
2021-12-01 15:59:31 +0700

    Refactor transaction item and add comments

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -64,7 +64,7 @@ function Cart() {
         "September",
         "October",
         "November",
-        "Dececember",
+        "December",
       ];
 
       const newTransaction = {
--- src/pages/HistoryTransaction/index.js
+++ src/pages/HistoryTransaction/index.js
@@ -16,47 +16,60 @@ function HistoryTransaction() {
     return transactions.map((trx) => {
       return (
         <div className="transaction-item">
+          {/* merah - start */}
           <div className="transaction-item-info1">
-            <strong>Belanja</strong>{" "}
-            {`${trx.date} ${trx.monthWord} ${trx.year}`}
-            <span className="transaction-status">Selesai</span>{" "}
+            <strong>Belanja</strong>
+            {`${trx.transactionDate.date} ${trx.transactionDate.monthWord} ${trx.transactionDate.year}`}
+            <span className="transaction-status">Selesai</span>
             {trx.invoiceNumber}
           </div>
+          {/* merah - end */}
           <div>
             <div className="d-flex justify-content-between">
-              <div>
-                {trx.transactionItems.map((item) => (
-                  <div>
-                    <p>Emmerce</p>
+              {/* kuning - start */}
 
-                    <div className="d-flex">
-                      <img
-                        className="transaction-img"
-                        src={item.productImage}
-                      />
-                      <div className="ms-3 transaction-info2">
-                        <p>
-                          <strong>{item.productName}</strong>
-                        </p>
-                        <p className="support">
-                          {item.quantity} barang x Rp. {item.price}
-                        </p>
+              <div>
+                {trx.transactionItems.map((item) => {
+                  // ungu - start
+                  return (
+                    <div>
+                      <p>Emmerce</p>
+
+                      <div className="d-flex">
+                        <img
+                          className="transaction-img"
+                          src={item.productImage}
+                        />
+                        <div className="ms-3 transaction-info2">
+                          <p>
+                            <strong>{item.productName}</strong>
+                          </p>
+                          <p className="support">
+                            {item.quantity} barang x Rp. {item.price}
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  );
+                  // ungu - start
+                })}
               </div>
+              {/* kuning - end */}
+              {/* hijau - start */}
               <div className="transaction-total-price d-flex flex-column justify-content-center">
                 <p>Total belanja</p>
                 <p>
                   <strong> Rp. {trx.totalPayment} </strong>
                 </p>
               </div>
+              {/* hijau - end */}
             </div>
 
+            {/* hitam - start */}
             <div className="transaction-detail-link">
               <span>Lihat detail transaksi</span>
             </div>
+            {/* hitam - end */}
           </div>
         </div>
       );
